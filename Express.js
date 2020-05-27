8d55009780f4caa3211d9a6a0383038e480d9a75
grabinow2 <grabinow2@student.touro.edu>
2020-05-26 20:30:12 -0400

    Deleted extraneous files, started working on DB connections.

--- Express.js
+++ Express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname+'/public'));
 app.route('/home').get(function(req,res)
@@ -18,3 +19,40 @@ app.get('/',function(req,res){
     res.render('home')
 });
 var server=app.listen(3000,function() {});
+
+
+function getStars() {
+    var con = mysql.createConnection({
+        host: "localhost",
+        username: "root",
+        password: "",
+        database: "MyStar",
+
+    });
+
+    con.connect(function () {
+
+        if(err) throw err;
+        else{
+            console.log("connected!");
+        }
+
+        var sql = "select * from star";
+
+        con.query(sql, function(err, result, fields){
+            if (err){
+                console.log("err");
+                throw err;
+            }
+            for(var i = 0; i < result.length; i++){
+                                
+            }
+
+            console.log("done looping");
+            //return classes;
+
+        });
+
+    })
+
+}
